9ce73779db6474100104090c03d462e7e60e549c
Steven Graboski <sgrabosk@gmail.com>
2016-05-02 16:56:05 -0400

    Init commit

---
+++ server.js
@@ -0,0 +1,16 @@
+/* Burger
+ *  -Steve G, Rutgers Coding Bootcamp
+ * ================================== */
+
+// express
+var express = require('express');
+var app = express();
+
+// handlebars
+var handlebars = require('express-handlebars');
+
+// body parser
+var bodyParser = require('body-parser');
+
+// method override
+var methodOverride = require('method-override');
