119b401c140634c5a22e7c835fc6c03671f1730f
Steven Graboski <sgrabosk@gmail.com>
2016-05-09 16:41:58 -0400

    changes to connection.js to connect to remote db'

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,24 @@
 var mysql = require('mysql');
 
 // specify connection
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	database: 'burgers_db',
-	user: 'root',
-	password: '',
-})
+var connection = {
+	local: {
+		port: 3306,
+		host: 'localhost',
+		database: 'burgers_db',
+		user: 'root',
+		password: '',
+	},
+	heroku: {
+		port: 3306,
+		host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		database: 'i0iuqapufsqpar1r',
+		user: 'xqsognnmdnhkwsod',
+		password: 'asqkg3pcf7nf9w7m',
+	}
+}
+
+mysql.createConnection(connection.heroku);
 
 // make the connection
 connection.connect(function(err) {
